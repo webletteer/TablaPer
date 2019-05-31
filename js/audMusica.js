c4be1cbc1342f780b4e8d49948257bc7d92aa13e
webletteer <50562051+webletteer@users.noreply.github.com>
2019-05-30 23:19:52 -0500

    Add files via upload

---
+++ js/audMusica.js
@@ -0,0 +1,35 @@
+
+
+
+function cambia(radio){
+var color=radio.name;
+document.getElementById("dato").style.background=color;
+}
+function cambia2(radio){
+var color=radio.name;
+document.getElementById("dato2").style.background=color;
+}
+function cambia3(radio){
+var color=radio.name;
+document.getElementById("dato3").style.background=color;
+}
+function cambia4(radio){
+var color=radio.name;
+document.getElementById("dato4").style.background=color;
+}
+function cambia5(radio){
+var color=radio.name;
+document.getElementById("dato5").style.background=color;
+}
+function cambia6(radio){
+var color=radio.name;
+document.getElementById("dato6").style.background=color;
+}
+function cambia7(radio){
+var color=radio.name;
+document.getElementById("dato7").style.background=color;
+}
+function cambia8(radio){
+var color=radio.name;
+document.getElementById("dato8").style.background=color;
+}
